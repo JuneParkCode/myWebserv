a608ba95728aeeaef1ad6e5356de39f1b2f3dd47
Photogrammer <khsj15@khu.ac.kr>
2022-12-15 02:51:56 +0900

    request 메소드 구현

--- HTTP/Request.cpp
+++ HTTP/Request.cpp
@@ -1,2 +1,106 @@
 #include "Request.hpp"
 
+const std::string& HTTP::Request::getMethod() const
+{
+  return m_method;
+}
+
+void HTTP::Request::setMethod(const std::string& mMethod)
+{
+  m_method = mMethod;
+}
+
+const std::string& HTTP::Request::getUrl() const
+{
+  return m_URL;
+}
+
+void HTTP::Request::setUrl(const std::string& mUrl)
+{
+  m_URL = mUrl;
+}
+
+const std::string& HTTP::Request::getVersion() const
+{
+  return m_version;
+}
+
+void HTTP::Request::setVersion(const std::string& mVersion)
+{
+  m_version = mVersion;
+}
+
+const std::map<std::string, std::string>& HTTP::Request::getHeaders() const
+{
+  return m_headers;
+}
+
+const WS::Storage& HTTP::Request::getBody() const
+{
+  return m_body;
+}
+
+void HTTP::Request::setBody(const WS::Storage& mBody)
+{
+  m_body = mBody;
+}
+
+ssize_t HTTP::Request::getContentLength() const
+{
+  return m_contentLength;
+}
+
+void HTTP::Request::setContentLength(ssize_t mContentLength)
+{
+  m_contentLength = mContentLength;
+}
+
+const std::chrono::system_clock& HTTP::Request::getRequestTime() const
+{
+  return m_requestTime;
+}
+
+void HTTP::Request::setRequestTime(const std::chrono::system_clock& mRequestTime)
+{
+  m_requestTime = mRequestTime;
+}
+
+FileDescriptor HTTP::Request::getConnectedSocketFd() const
+{
+  return m_connectedSocketFD;
+}
+
+void HTTP::Request::setConnectedSocketFd(FileDescriptor mConnectedSocketFd)
+{
+  m_connectedSocketFD = mConnectedSocketFd;
+}
+
+FileDescriptor HTTP::Request::getReadFd() const
+{
+  return m_readFD;
+}
+
+void HTTP::Request::setReadFd(FileDescriptor mReadFd)
+{
+  m_readFD = mReadFd;
+}
+
+FileDescriptor HTTP::Request::getWriteFd() const
+{
+  return m_writeFD;
+}
+
+void HTTP::Request::setWriteFd(FileDescriptor mWriteFd)
+{
+  m_writeFD = mWriteFd;
+}
+
+const HTTP::Response& HTTP::Request::getResponse() const
+{
+  return m_response;
+}
+
+void HTTP::Request::setResponse(const HTTP::Response& mResponse)
+{
+  m_response = mResponse;
+}
--- HTTP/Request.hpp
+++ HTTP/Request.hpp
@@ -13,9 +13,6 @@ namespace HTTP
   class Request
   {
   private:
-      std::string m_method;
-      std::string m_URL;
-      std::string m_version;
       std::map<std::string, std::string> m_headers;
       WS::Storage m_body;
       ssize_t m_contentLength;
@@ -24,12 +21,32 @@ namespace HTTP
       FileDescriptor m_readFD;
       FileDescriptor m_writeFD;
       HTTP::Response m_response;
+      std::string m_method;
+      std::string m_URL;
+      std::string m_version;
   public:
-      const std::string& getHeader(const std::string& key) const;
-      const WS::Storage& getBody() const;
       const std::string& getMethod() const;
-      const std::string& getURL() const;
+      void setMethod(const std::string& mMethod);
+      const std::string& getUrl() const;
+      void setUrl(const std::string& mUrl);
       const std::string& getVersion() const;
+      void setVersion(const std::string& mVersion);
+      const std::map<std::string, std::string>& getHeaders() const;
+      const WS::Storage& getBody() const;
+      void setBody(const WS::Storage& mBody);
+      ssize_t getContentLength() const;
+      void setContentLength(ssize_t mContentLength);
+      const std::chrono::system_clock& getRequestTime() const;
+      void setRequestTime(const std::chrono::system_clock& mRequestTime);
+      FileDescriptor getConnectedSocketFd() const;
+      void setConnectedSocketFd(FileDescriptor mConnectedSocketFd);
+      FileDescriptor getReadFd() const;
+      void setReadFd(FileDescriptor mReadFd);
+      FileDescriptor getWriteFd() const;
+      void setWriteFd(FileDescriptor mWriteFd);
+      const Response& getResponse() const;
+      void setResponse(const Response& mResponse);
+  public:
       HTTP::Response& response();
       bool isTimeout();
       bool isFinished();
